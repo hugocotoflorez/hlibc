904f97b4b3c30e9bd103365e866b6c829f47df2a
hugocotoflorez <hugocoto100305@gmail.com>
2024-09-26 17:35:16 +0200

    update malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -1,227 +1,482 @@
 #include "malloc.h"
-#include "../string/string.h" // memmove
-#include <sys/mman.h>         // mmap, munmap
+#include <errno.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h> // mmap, munmap
 
-// global reference to free list (sorted linked list started at head)
-node_t *head = NULL;
+#include <stdio.h>
+/* See malloc.h for further information about
+ * how this shit work and interface usage for
+ * programmers. */
 
+/* head is the first page.
+ * If head is NULL there are no pages initialized */
+static page_t *head = NULL;
 
-void
-__alloc_destroy()
+#ifdef ALLOC_TEST
+/* Unused variable, just for print info (at testing) */
+static unsigned page_count = 0;
+
+/* Just used to pass head to test.c */
+inline void *
+__debug_get_head()
 {
-    munmap(head, SIZE);
-    head = NULL;
+    return head;
+}
+#endif
+
+static page_t *
+get_last_page()
+{
+    /* I use a linear search instead of a direct pointer
+     * because most times there are a few pages, so
+     * a loop over a few nodes is harmless and 8bytes are
+     * allways 8 bytes (Hugo's lazzy approach #2). */
+    page_t *pptr = head;
+
+    /* No pages initialized */
+    if (pptr == NULL)
+        return NULL;
+
+    while (pptr->next != NULL)
+        pptr = pptr->next;
+
+    return pptr;
 }
 
-__attribute__((constructor)) static void // call function before main
-__init__()
+/* Get the free block node before PTR.
+ * PTR is a valid address in the page
+ * pointed by PPTR.*/
+static void *
+get_prev_node(page_t *pptr, void *ptr)
 {
-    // avoid multiple initializations
-    if (head == NULL)
-    {
-        // mmap() return a block of free space
-        head =
-        mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    node_t *nptr = (node_t *) (pptr + 1);
 
-        head->size = SIZE - sizeof(node_t);
-        head->next = NULL;
+    /* Note for debugging:
+     * A SEGFAULT in the next line is not
+     * caused by this funcion, something is
+     * not assigned correctly so nptr->next
+     * never becames NULL as espected.
+     * Trust me <3 */
+    while (nptr->next && (void *) nptr->next < ptr)
+        nptr = nptr->next;
+
+    return nptr;
+}
+
+static node_t *
+get_best_fit(size_t size)
+{
+    /* Fit algorithm
+     * Note: algorithm can be changed by
+     * changing this funcion implementation
+     *
+     * First smallest block algorithm.
+     * This algorithm return the smallest block
+     * of SIZE or more bytes in the first page
+     * with a block that big.
+     *
+     * Its quite inefficient but simple.
+     */
+    page_t *pptr;
+    node_t *nptr;
+    node_t *best_fit = NULL;
+    size_t  real_size;
+
+    /* Go througth page list */
+    for (pptr = head; pptr != NULL; pptr = pptr->next)
+    {
+        /* first node into page */
+        nptr = (node_t *) ((char *) pptr + sizeof(page_t));
+
+        for (; nptr != NULL; nptr = nptr->next)
+        {
+            real_size = nptr->size + sizeof(header_t);
+            /* Best fit changes if size fits in current node and:
+             * - best fit is not assigned yet
+             * - new node size is smaller than best fit size */
+            if (real_size >= size && (!best_fit || real_size < best_fit->size))
+                best_fit = nptr;
+        }
+        if (best_fit != NULL)
+            return best_fit;
+    }
+    return NULL;
+}
+
+/* Return a page to os. All its contents would be lost.
+ * Have to be called once all page contents are free.
+ * PAGE is deleted from pages list and head updated if needed.
+ * PAGE is set to NULL on success.
+ */
+static void
+__page_destroy(page_t *page)
+{
+    if (page == NULL || page->magic != MAGIC)
+        return;
+
+    /* Unlink page */
+    if (page->next)
+        page->next->prev = page->prev;
+
+    if (page->prev)
+        page->prev->next = page->next;
+
+    /* Update head if needed */
+    else
+        head = page->next;
+
+    /* Free current page */
+    munmap(page, page->size);
+}
+
+/* Ask for a new page and update PAGE to the new reserved memory.
+ * New chunk is formated with page and header, ready to use.
+ * If PAGE is not NULL at function call, PAGE is returned without
+ * any action or change.
+ * If PAGE is NULL an still NULL at return, some error happened.
+ */
+static void *
+__page_create(size_t size)
+{
+    node_t *nptr;
+    page_t *pptr;
+
+    pptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+
+    /* Page can not be allocated */
+    if (pptr == MAP_FAILED)
+    {
+        /* Set errno as OUT OF MEMORY */
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    /* Fill pptr header */
+    *pptr = (page_t){
+        .next  = NULL,
+        .prev  = get_last_page(),
+        .size  = size,
+        .magic = MAGIC,
+#ifdef ALLOC_TEST
+        .page_id = page_count++,
+#endif
+    };
+
+    /* Create first node */
+    nptr  = (node_t *) (pptr + 1);
+    *nptr = (node_t){
+        .page = pptr,
+        .size = pptr->size - sizeof(page_t) - sizeof(node_t),
+        .next = NULL,
+        .prev = NULL,
+    };
+
+    /* Link current pptr */
+    if (pptr->prev)
+        pptr->prev->next = pptr;
+
+    /* If first pptr update head */
+    else
+        head = pptr;
+
+    return pptr;
+}
+
+static void
+destroy_all_pages()
+{
+    page_t *pptr = head;
+    page_t *next = NULL;
+
+    /* Go througth page list */
+    for (; pptr != NULL; pptr = next)
+    {
+        next = pptr->next;
+        __page_destroy(pptr);
     }
 }
 
-void
-free(void *ptr)
+__attribute__((constructor)) static void
+__init__()
 {
-    header_t *hptr;     // header of ptr
-    node_t   *prev;     // previous free-node
-    node_t   *new_node; // new node to add
+    atexit(destroy_all_pages);
+}
 
+void
+__free(void *ptr)
+{
+    header_t *hptr;
+    node_t   *nptr;
+    header_t  temp_header;
+
+    /* Avoid NULL as argument */
     if (ptr == NULL)
         return;
 
-    hptr = ptr - sizeof(header_t);
+    hptr = (header_t *) ptr - 1;
 
-    // check if hptr is valid
-    if (hptr->magic != MAGIC)
+    /* Check if it is not valid */
+    if (hptr == NULL || hptr->magic != MAGIC)
         return;
 
-    // unset header as valid
+    /* Unset header as valid */
     hptr->magic = 0;
 
-    // new node to add
-    new_node       = (node_t *) hptr;
-    new_node->size = hptr->size + sizeof(header_t) - sizeof(node_t);
+    /* New node to add */
+    temp_header = *hptr;
+    nptr        = (node_t *) hptr;
+    nptr->size  = temp_header.size + sizeof(header_t) - sizeof(node_t);
+    nptr->page  = temp_header.page;
+    nptr->prev  = get_prev_node(hptr->page, ptr);
 
-    // get prev node
-    prev = head;
-    while (prev->next != NULL && prev->next < new_node)
-        prev = prev->next;
+    /* Insert new node */
+    nptr->next = nptr->prev->next;
+    if (nptr->next)
+        nptr->next->prev = nptr;
+    if (nptr->prev)
+        nptr->prev->next = nptr;
 
-    // insert new node into list
-    new_node->next = prev->next;
-    prev->next     = new_node;
 
-    // join with next node if also free
-    if ((char *) new_node + sizeof(node_t) + new_node->size ==
-        (char *) new_node->next)
+    /* join with next node if also free */
+    if ((char *) nptr + sizeof(node_t) + nptr->size == (char *) nptr->next)
     {
-        new_node->size += new_node->next->size + sizeof(node_t);
-        new_node->next = new_node->next->next;
+        nptr->size += nptr->next->size + sizeof(node_t);
+        nptr->next = nptr->next->next;
     }
 
-    // join with previous node if also free
-    if ((char *) prev + sizeof(node_t) + prev->size == (char *) new_node)
+    /* join with previous node if also free */
+    if ((char *) nptr->prev + sizeof(node_t) + nptr->prev->size == (char *) nptr)
     {
-        prev->size += new_node->size + sizeof(node_t);
-        prev->next = new_node->next;
+        nptr->prev->size += nptr->size + sizeof(node_t);
+        nptr->prev->next = nptr->next;
     }
 
-    // if any memory is used, return it to os
-    if (prev->size == SIZE - sizeof(node_t))
+    /* if any memory is used, return it to os */
+    if (nptr->prev->size == nptr->page->size - sizeof(node_t) - sizeof(page_t))
     {
-        __alloc_destroy();
+        __page_destroy(nptr->page);
     }
 }
 
 void *
-malloc(int size)
+__malloc(size_t size)
 {
-    node_t *temp      = head;
-    void   *ptr       = NULL;
-    int     best_size = SIZE;
+    header_t *hptr;
+    node_t   *nptr;
+    page_t   *pptr;
+    size_t    realsize;
+    size_t    greater_size_needed;
+    size_t    block_size = DEFAULT_PAGE_SIZE;
 
-    // check if it is initialized
-    if (head == NULL)
-        __init__();
+    /* SIZE with both page and block headers */
+    greater_size_needed = size + sizeof(page_t) + sizeof(header_t);
 
-    // check if size is valid
-    if (size <= 0)
+    /* Invalid size checks */
+    if (size == 0 || greater_size_needed > MAX_ALLOC_SIZE)
         return NULL;
 
-    // if size is smaller than headers diff new header dont have enought space at free
-    if (size < sizeof(node_t) - sizeof(header_t))
+    /* Total size, requested size + header size */
+    realsize = size + sizeof(header_t);
+
+    /* Find a free node to allocate SIZE bytes */
+    nptr = get_best_fit(realsize);
+
+    /* If cant be allocated in any page, create a new one */
+    if (nptr == NULL)
     {
-        size = sizeof(node_t) - sizeof(header_t);
+        /* Get a block size with at least SIZE bytes avaliable (or max size) */
+        while (greater_size_needed >= block_size && block_size < MAX_ALLOC_SIZE)
+            block_size <<= 1;
+
+        /* Error at page creation */
+        if ((pptr = __page_create(block_size)) == NULL)
+            return NULL;
+
+        /* node is first page node */
+        nptr = (node_t *) (pptr + 1);
     }
 
-    // best fit algorithm (smallest free space greater or equal than size)
-    // done in a linear way, end in O(n) algorithm
-    do
-    {
-        if (temp->size >= size && best_size > temp->size)
-        {
-            best_size = temp->size;
-            ptr = (void *) temp; // ptr is the list entry that best fit
-        }
-        temp = temp->next;
-
-    } while (temp != NULL);
-
-    // if pointer fits in memory
-    if (ptr != NULL)
-    {
-        // update free list
-        ((node_t *) ptr)->size -= size + sizeof(header_t);
-        // move pointer to last free 'size' bytes in free space
-        ptr = (char *) ptr + best_size - size + sizeof(node_t);
-        // add header to allocated memory
-        *((header_t *) ptr - 1) = (header_t){ .size = size, .magic = MAGIC };
-    }
-    // return ptr or NULL if cant allocate that memory ammount
-    return ptr;
-}
-
-
-void *
-realloc_after(void *ptr, int size, header_t *hptr)
-{
-    void     *next        = (char *) ptr + hptr->size;
-    header_t *hnext       = next;
-    node_t   *nnext       = next;
-    int       needed_size = size - hptr->size;
-    node_t   *prev;
-
-    if (hnext->magic == MAGIC) // next block is used
-        return NULL;
-
-    // create next node
-    if (nnext->size > needed_size)
-    {
-        *(node_t *) (next + needed_size) = (node_t){
-            .size = nnext->size - needed_size,
-            .next = nnext->next,
-        };
-
-        hptr->size += needed_size;
-
-        // get prev node
-        prev = head;
-        while (prev->next != NULL && (void *) prev->next < ptr)
-            prev = prev->next;
-        prev->next = next + needed_size;
-
-        return ptr;
-    }
+    /* If it fits, selected page is nptr page */
     else
-        return NULL;
+        pptr = nptr->page;
+
+    /* set hptr as the last SIZE bytes of hptr */
+    hptr = (header_t *) ((char *) nptr + sizeof(node_t) + nptr->size - realsize);
+
+    *hptr = (header_t){
+        .size  = size,
+        .magic = MAGIC,
+        .page  = pptr,
+    };
+
+    nptr->size -= realsize;
+
+    return (hptr + 1);
 }
 
 void *
-realloc_before(void *ptr, int size, header_t *hptr)
+__calloc(size_t nmemb, size_t size)
 {
-    node_t *prev;
+    void *ret = __malloc(nmemb * size);
 
-    // get prev node
-    prev = head;
-    while (prev->next != NULL && (void *) prev->next < ptr)
-        prev = prev->next;
-
-    // check if previous free block is just before current block
-    if ((char *) prev + prev->size == (char *) hptr)
-    {
-        prev->size -= size - hptr->size;
-        *(header_t *) ((char *) hptr - (size - hptr->size)) = (header_t){
-            .size  = size,
-            .magic = MAGIC,
-        };
-        memmove((char *) hptr - (size - hptr->size) + sizeof(header_t), ptr, size);
-    }
+    if (ret)
+        return memset(ret, 0, nmemb * size);
 
     return NULL;
 }
 
 void *
-realloc(void *ptr, int size)
+realloc_use_next(void *ptr, size_t size, header_t *hptr)
+{
+    /* +------------------------+
+     * | Used                   | <- ptr
+     * +------------------------+
+     * | Free                   | <- next
+     * +------------------------+
+     * First block use part of the second, and node is moved.
+     */
+    void     *next;
+    header_t *hnext;
+    node_t    nnext;
+    size_t    needed_size;
+    node_t   *nnode;
+
+    next = (char *) ptr + hptr->size;
+
+    /* Check if next is out of page */
+    if ((char *) next >= (char *) hptr->page + hptr->page->size)
+        return NULL;
+
+
+    /* Next block is not free */
+    hnext = (header_t *) next;
+    if (hnext->magic == MAGIC)
+        return NULL;
+
+    needed_size = size - hptr->size;
+    nnext       = *(node_t *) next;
+
+    /* Invalid size (not enought) */
+    if (nnext.size < needed_size)
+        return NULL;
+
+    /* Create new node for next (moved) free block */
+    nnode  = (node_t *) ((char *) next + needed_size);
+    *nnode = (node_t){
+        .size = nnext.size - needed_size,
+        .next = nnext.next,
+        .page = nnext.page,
+        .prev = nnext.prev,
+    };
+
+    /* Link new node */
+    if (nnode->prev)
+        nnode->prev->next = nnode;
+    if (nnode->next)
+        nnode->next->prev = nnode;
+
+    /* Update header size */
+    hptr->size += needed_size;
+
+    return ptr;
+}
+
+void *
+realloc_use_prev(void *ptr, int size, header_t *hptr)
+{
+    /* +------------------------+
+     * | Free                   | <- prev
+     * +------------------------+
+     * | Used                   | <- ptr
+     * +------------------------+
+     * Second block use part of the first, and header is moved.
+     */
+    header_t *nhptr; /* New header address */
+    node_t   *prev;
+
+    nhptr = (header_t *) ((char *) hptr - (size - hptr->size));
+    prev  = get_prev_node(hptr->page, ptr);
+
+    /* check if previous free block is just before current block */
+    if ((char *) (prev + 1) + prev->size != (char *) hptr)
+        return NULL;
+
+    /* Block is to big to realloc here */
+    if (prev->size < size - hptr->size)
+        return NULL;
+
+    /* Update prev free node size */
+    prev->size -= size - hptr->size;
+
+    /* Create (move) header */
+    *nhptr = (header_t){
+        .size  = size,
+        .magic = MAGIC,
+        .page  = hptr->page,
+    };
+
+    /* Copy data */
+    memmove(nhptr + 1, ptr, size);
+
+    return nhptr + 1;
+}
+
+void *
+__realloc(void *ptr, size_t size)
 {
     void     *newptr = NULL;
     header_t *hptr;
 
-    if (head == NULL) // check if it is initialized
-        __init__();
+    /* Any page initialized */
+    if (head == NULL)
+    {
+        if (size)
+            return __malloc(size);
+        else
+            return NULL;
+    }
 
-    if (ptr == NULL) // allow use rehalloc as mhalloc
-        return malloc(size);
+    /* Allow use realloc as malloc */
+    if (ptr == NULL)
+        return __malloc(size);
 
-    hptr = ptr - sizeof(header_t); // get header
+    /* Allow use realloc as free */
+    if (size == 0)
+        return __free(ptr), NULL;
 
-    if (hptr->size >= size) // if size is invalid return NULL
+    /* Header of block to realloc */
+    hptr = (header_t *) ptr - 1;
+
+    /* New size is smaller or equal than previous one */
+    if (hptr->size >= size)
         return NULL;
 
-    // try to allocate just after
-    if ((newptr = realloc_after(ptr, size, hptr)) != NULL)
+    /* Try to allocate just after */
+    newptr = realloc_use_next(ptr, size, hptr);
+    if (newptr)
         return newptr;
 
-    // try to allocate just before
-    if ((newptr = realloc_before(ptr, size, hptr)) != NULL)
+    /* Try to allocate just before */
+    newptr = realloc_use_prev(ptr, size, hptr);
+    if (newptr)
         return newptr;
 
+    /* A third case can happened, when using previous node
+     * and next node at the same. As this operation is hard
+     * to implement and hard to happend, a default realloc
+     * is done. (Hugo's lazzy approach #3). */
 
-    // default rehalloc
-    // allocate new block and move data, then free current block
-    if ((newptr = malloc(size)) == NULL) // check for out-of-memmory error
+    /* Default: Allocate new block and move data, then free current block */
+
+    newptr = __malloc(size);
+    if (!newptr)
+        /* out-of-memory */
         return NULL;
 
-    memmove(newptr, ptr, hptr->size); // move data
-    free(ptr);
+    memmove(newptr, ptr, hptr->size);
+    __free(ptr);
 
     return newptr;
 }
--- malloc/malloc.h
+++ malloc/malloc.h
@@ -1,65 +1,181 @@
 #ifndef _HALLOCATOR
 #define _HALLOCATOR
 
-#include "../stddef/stddef.h" // size_t
+#include <stddef.h> // size_t
 
+#define ALLOC_TEST
+
+
+#ifdef ALLOC_TEST
+void *__debug_get_head();
+#endif
 /*
  * Memory management library
- * Something as in stdlib
- *
- * Author: Hugo Coto Florez
+ * by Hugo Coto Florez
  *
  * Inspiration: ostep book
  */
 
-// total chunk size
-#define SIZE 4096
+/* Info for code readers
+ *
+ * Name conventions:
+ * - ptr: (void*) no relevant info
+ * - hptr: (header_t*) current block header
+ * - pptr: (page_t*) current page header
+ * - nptr: (node_t*) current node header
+ *
+ * I hope code is as readable as possible. Any doubt
+ * about what is happening can be asked directly to
+ * myself (contact info in github profile) or oppening
+ * an issue.
+ *
+ * Althought gcc let do arithmetic over void*, as C standard
+ * said that void pointer arithmetic has undefined behaviour
+ * I cast to char* all this cases.
+ *
+ * I appreciate any contribution, from finding a new bug
+ * to implementing a new feature. Thanks for your time.
+ */
 
-#define MAGIC 0x777
+/* Info about low level mechanisms
+ *
+ * Page:
+ * Pages are raw chunks of data recived thow a mmap() call.
+ * It contains a page header (page_t) at the LSB byte.
+ * Remaining block space can be one of the following entries.
+ *
+ * Node:
+ * A node (node_t) is the structure that store info
+ * about a free block. It is placed just before the
+ * free block.
+ *
+ * Header:
+ * A header (header_t) that contains info about reserved memory.
+ * After the header start an used block.
+ *
+ * Free block.
+ * Data or nothing that is not allocated neither used
+ * by any header. Just before this block there are a
+ * node (node_t).
+ *
+ * Used block.
+ * Data that where allocated. Just before this block
+ * there are a header (header_t).
+ */
 
-typedef struct __node_t
+/* Page size is by default DEFAULT_PAGE_SIZE, up to MAX_ALLOC_SIZE */
+#define DEFAULT_PAGE_SIZE 0x1p12 /* 4096 */
+#define MAX_ALLOC_SIZE    0x1p14 /* 16384 */
+
+/* Magic number. Used to check integrity */
+#define MAGIC 0x123456
+/* As pages are accesed by address and headers by offset, I use the
+ * same magic number. I think its nearly impossible to access a page
+ * with a header or viceversa. Hugo's lazzy approach #1 */
+
+typedef struct __page_t page_t;
+typedef struct __node_t node_t;
+
+/* Above each free block a node_t node is placed. */
+struct __node_t
 {
-    size_t           size;
-    struct __node_t *next;
-} node_t;
+    size_t  size; // free block size in bytes (without node_t size)
+    node_t *next; // next free node in the same page, or NULL if last
+    node_t *prev; // previous free node in the same page, or NULL if first
+    page_t *page; // page header of page that nide is in
+};
 
+/* First data of each page. */
+struct __page_t
+{
+    page_t  *next;    // next page
+    page_t  *prev;    // prev page
+    size_t   size;    // page size (with page header)
+    unsigned magic;   // used to check if ptr is page_t
+#ifdef ALLOC_TEST
+    unsigned page_id; // page number
+#endif
+};
+
+/* Header above used memory */
 typedef struct
 {
-    size_t size;
-    int    magic;
+    size_t   size;  // allocated bytes without header size
+    unsigned magic; // magic number to check header
+    page_t  *page;  // page header of page that nide is in
 } header_t;
 
-/*
- * void *mhalloc(int size)
- * Alloc 'size' bytes and return a pointer to the first byte
- * If cant alloc, return NULL
+
+/* Interfaces provided to user.
+ * Notes about implentation can be found in malloc.c */
+
+/* The  free()  function  frees the memory space pointed to by ptr,
+ * which must have been returned by a previous call to malloc()  or
+ * related functions.  Otherwise, or if ptr has already been freed,
+ * undefined behavior occurs.  If ptr is NULL, no operation is performed.
+ * Return no value, and preserve errno.
  */
-void *malloc(size_t size);
+void  __free(void *ptr);
+
+/* The malloc() function allocates size bytes and returns a pointer
+ * to  the  allocated  memory.   The memory is not initialized.
+ * Size must be grater than 0 (Non-gnu approach).
+ *
+ * Return a pointer to the  allocated  memory,  which  is  suitably
+ * aligned  for any type that fits into the requested size or less.
+ * On error, these functions return NULL and set errno.  Attempting
+ * to allocate more than PTRDIFF_MAX bytes is considered an  error,
+ * as an object that large could cause later pointer subtraction to
+ * overflow.
+ */
+void *__malloc(size_t size);
+
+/* The calloc() function allocates memory for an array of nmemb elements
+ * of size bytes each and returns a pointer to the allocated
+ * memory.  The memory is set to zero. Size must be grater than 0 (Non-gnu approach).
+ *
+ * Return a pointer to the  allocated  memory,  which  is  suitably
+ * aligned  for any type that fits into the requested size or less.
+ * On error, these functions return NULL and set errno.  Attempting
+ * to allocate more than PTRDIFF_MAX bytes is considered an  error,
+ * as an object that large could cause later pointer subtraction to
+ * overflow.
+ */
+
+void *__calloc(size_t nmemb, size_t size);
 
 /*
- * void *rehalloc(void* ptr, int size)
- * Alloc 'size' bytes and move data from ptr to a new possition,
- * returns a pointer to a new possition and free previous pointer.
- * On error previous pointer is not free and return NULL
+ * The  realloc()  function  changes  the  size of the memory block
+ * pointed to by ptr to size bytes.  The  contents  of  the  memory
+ * will  be  unchanged in the range from the start of the region up
+ * to the minimum of the old and new sizes.  If  the  new  size  is
+ * larger  than the old size, the added memory will not be initialized.
+ *
+ * If ptr is NULL, then the call is equivalent to malloc(size), for
+ * all values of size.
+ *
+ * If size is equal to zero, and ptr is not NULL, then the call  is
+ * equivalent  to  free(ptr).
+ *
+ * Unless ptr is NULL, it must have been  returned  by  an  earlier
+ * call to malloc or related functions.  If the area pointed to was
+ * moved, a free(ptr) is done.
+ *
+ * Return a pointer to the  allocated  memory,  which  is  suitably
+ * aligned  for any type that fits into the requested size or less.
+ * On error, these functions return NULL and set errno.  Attempting
+ * to allocate more than PTRDIFF_MAX bytes is considered an  error,
+ * as an object that large could cause later pointer subtraction to
+ * overflow.
+ * The realloc() function return NULL if ptr is
+ * not  NULL and the requested size is zero; this is not considered
+ * an error.
+ * Otherwise,  the  returned  pointer may be the same as ptr if the
+ * allocation was not moved (e.g., there was room to expand the allocation in-place),
+ * or different from ptr if the allocation  was
+ * moved  to  a new address.  If these functions fail, the original
+ * block is left untouched; it is not freed or moved.
  */
-void *realloc(void *ptr, size_t size);
-
-/*
- * void fhree(void* ptr)
- * Free memory pointed by ptr
- */
-void free(void *ptr);
-
-/*
- * Automatically initialized before main(), can also be
- * called, it checks if it was initialized before
- */
-void __halloc_init();
-
-/*
- * Automatically call at exit, can also be called manually
- * but calls to allocator after this function would end in a segfault
- */
-void __alloc_destroy();
+void *__realloc(void *ptr, size_t size);
 
 #endif // !_HALLOCATOR
