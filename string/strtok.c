81e405fdc5d080be808d764fb0ad4a8b3b9a2373
hugocotoflorez <hugocoto100305@gmail.com>
2024-10-11 18:01:59 +0200

    strtok? idk

--- string/strtok.c
+++ string/strtok.c
@@ -1,15 +1,26 @@
 #include "string.h"
 
+static char *__strtok_next;
+
+/* Very important message!!!!
+ * This funcion dont work.
+ * I think it does but as I
+ * didnt test it I assume it
+ * dont work as it should */
 char *
 strtok(char *s, const char *delim)
 {
-    while (*s)
-    {
-        if (!strcmp(s, delim))
-        {
-            *s = '\0';
-            return ++s;
-        }
-    }
-    return NULL;
+    char *start;
+
+    if (s == NULL)
+        s = __strtok_next;
+
+    while (*s && strcmp(s, delim))
+        ++s;
+
+    *s = 0;
+    s += strlen(delim);
+    __strtok_next = s;
+
+    return start;
 }
